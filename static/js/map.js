458d231ed4995c62f7403ecc7dbcaae8c3a6add6
STIPCommonsLab <commonslab@wilsoncenter.org>
2015-04-21 14:18:23 -0400

    Update map.js

--- static/js/map.js
+++ static/js/map.js
@@ -1,3 +1,18 @@
+var hotelIcon = L.icon({
+    iconUrl: 'static/img/hotel.png',
+
+    iconSize:     [25, 30], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+var parkingIcon = L.icon({
+    iconUrl: 'static/img/parking.png',
+
+    iconSize:     [25, 30], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 
 var map = L.map('map',{
         center: new L.LatLng(38.894487, -77.042562),
